38ef0330adbe4187961067faa1424fe458c43817
jedena <blackmlm@gmail.com>
2025-04-07 11:41:57 -0500

    update to 1.4
    Fixed to work with inkscape 1.4

--- extensions/connectthedots.py
+++ extensions/connectthedots.py
@@ -16,135 +16,156 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Fixed to work on Inkscape 1.4 and later by Jose Garza
+2025 - Fixed to work with inkscape 1.4
 '''
 
 from random import randint, choice
 from math import pi, acos, sqrt, degrees
 
 import inkex
-import simplepath
-import simplestyle
-import gettext
-_ = gettext.gettext
+from inkex import PathElement
+from inkex.paths import Path
 
-class ConnectTheDots(inkex.Effect):
+class ConnectTheDots(inkex.EffectExtension):
 	TOP_RIGHT = 1
 	TOP_LEFT = 2
 	BOTTOM_LEFT = 3
 	BOTTOM_RIGHT = 4
-	def __init__(self):
-		inkex.Effect.__init__(self)
-		self.OptionParser.add_option('-p', '--hidepath', action = 'store',
-									 type = 'string', dest = 'hidepath', default = True,
-									 help = 'Hide the original path after completion?')
-		self.OptionParser.add_option('-r', '--radius', action = 'store',
-									 type = 'string', dest = 'radius', default = 10,
-									 help = 'Radius of the dots at every vertex of the path')
-		self.OptionParser.add_option('-f', '--fontsize', action = 'store',
-									 type = 'string', dest = 'fontsize', default = 10,
-									 help = 'Font size of the numbers')
+	
+	def add_arguments(self, pars):
+		pars.add_argument('-p', '--hidepath', type=inkex.Boolean, default=True,
+						  help='Hide the original path after completion?')
+		pars.add_argument('-r', '--radius', type=float, default=10,
+						  help='Radius of the dots at every vertex of the path')
+		pars.add_argument('-f', '--fontsize', type=float, default=10,
+						  help='Font size of the numbers')
 
 
 	def effect(self):
-		svg = self.document.getroot()
+		svg = self.svg
 
 		# create new layers for dots and for numbers
-		dotLayer = inkex.etree.SubElement(svg, 'g')
-		dotLayer.set(inkex.addNS('label', 'inkscape'), 'ConnectTheDots dotLayer')
-		dotLayer.set(inkex.addNS('groupmode', 'inkscape'), 'layer')
-
-		numberLayer = inkex.etree.SubElement(svg, 'g')
-		numberLayer.set(inkex.addNS('label', 'inkscape'), 'ConnectTheDots numberLayer')
-		numberLayer.set(inkex.addNS('groupmode', 'inkscape'), 'layer')
+		dotLayer = svg.add(inkex.Layer.new('ConnectTheDots dotLayer'))
+		numberLayer = svg.add(inkex.Layer.new('ConnectTheDots numberLayer'))
 
 		# iterate over every path, start numbering from 1 every time
-		for id, path in self.selected.iteritems():
-			if path.tag == 'path' or path.tag == inkex.addNS('path','svg'):
-				# radius and fontsize as offsets to avoid placing numbers inside dots
-				r = float(self.options.radius)
-				f = 0.5*int(self.options.fontsize)
+		for path in self.svg.selection.filter(PathElement):
+			# radius and fontsize as offsets to avoid placing numbers inside dots
+			r = float(self.options.radius)
+			f = 0.5 * self.options.fontsize
 
-				# iterate over vertices and draw dots on each as well as the number next to it
-				d = path.get('d')
-				vertices = simplepath.parsePath(d)
-				for idx, v in enumerate(vertices):
-					x,y = self.getXY(v)
+			# iterate over vertices and draw dots on each as well as the number next to it
+			d = path.get('d')
+			path_object = Path(d)
+			vertices = path_object.to_arrays()
+			
+			for idx, v in enumerate(vertices):
+				x, y = self.getXY(v)
 
-					# create dots
-					style = {
-						'stroke': 'none',
-						'stroke-width': '0',
-						'fill': '#000000'
-					}
-					name = 'pbn_%i' % idx
-					attributes = {
-						'style': simplestyle.formatStyle(style),
-						inkex.addNS('cx', 'sodipodi') : str(x),
-						inkex.addNS('cy', 'sodipodi') : str(y),
-						inkex.addNS('rx', 'sodipodi') : self.options.radius,
-						inkex.addNS('ry', 'sodipodi') : self.options.radius,
-						inkex.addNS('start', 'sodipodi') : str(0),
-						inkex.addNS('end', 'sodipodi') : str(2*pi),
-						inkex.addNS('open', 'sodipodi') : 'false',
-						inkex.addNS('type', 'sodipodi') : 'arc',
-						inkex.addNS('label','inkscape') : name
-					}
-					dot = inkex.etree.SubElement(dotLayer, inkex.addNS('path', 'svg'), attributes)
+				# create dots
+				style = {
+					'stroke': 'none',
+					'stroke-width': '0',
+					'fill': '#000000'
+				}
+				name = 'pbn_%i' % idx
+				
+				# Create a circle element for the dot
+				circle = inkex.Circle()
+				circle.set('cx', str(x))
+				circle.set('cy', str(y))
+				circle.set('r', str(r))
+				circle.style = style
+				circle.set('id', name)
+				dotLayer.add(circle)
 
-					if idx > 0 and idx < len(vertices)-1:
-						# block two quadrants, one for the previous and one for the next
-						freeQuads = self.findFreeQuadrants((x,y), self.getXY(vertices[idx-1]), self.getXY(vertices[idx+1]))
+				if idx > 0 and idx < len(vertices)-1:
+					# block two quadrants, one for the previous and one for the next
+					freeQuads = self.findFreeQuadrants((x,y), self.getXY(vertices[idx-1]), self.getXY(vertices[idx+1]))
+				elif idx > 0:
+					# special case for end nodes, only block one quadrant
+					freeQuads = self.findFreeQuadrants((x,y), self.getXY(vertices[idx-1]))
+				else:
+					# special case for first node when it's the only one
+					if len(vertices) > 1:
+						freeQuads = self.findFreeQuadrants((x,y), self.getXY(vertices[idx+1]))
 					else:
-						# special case for end nodes, only block one quadrant
-						freeQuads = self.findFreeQuadrants((x,y), self.getXY(vertices[idx-1]))
+						# If there's only one vertex, use all quadrants
+						freeQuads = [self.TOP_LEFT, self.TOP_RIGHT, self.BOTTOM_RIGHT, self.BOTTOM_LEFT]
 
-					# randomly place number in one of the free quadrants
-					q = choice(freeQuads)
-					if q == self.TOP_RIGHT:
-						nx = x+2*r
-						ny = y+2*r+f
-						textAnchor = 'start'
-					elif q == self.TOP_LEFT:
-						nx = x-2*r
-						ny = y+r+f
-						textAnchor = 'end'
-					elif q == self.BOTTOM_LEFT:
-						nx = x-r
-						ny = y-r
-						textAnchor = 'end'
-					else: # BOTTOM_RIGHT
-						nx = x+r
-						ny = y-r
-						textAnchor = 'start'
+				# randomly place number in one of the free quadrants
+				q = choice(freeQuads)
+				if q == self.TOP_RIGHT:
+					nx = x+2*r
+					ny = y+2*r+f
+					textAnchor = 'start'
+				elif q == self.TOP_LEFT:
+					nx = x-2*r
+					ny = y+r+f
+					textAnchor = 'end'
+				elif q == self.BOTTOM_LEFT:
+					nx = x-r
+					ny = y-r
+					textAnchor = 'end'
+				else: # BOTTOM_RIGHT
+					nx = x+r
+					ny = y-r
+					textAnchor = 'start'
 
-					# create the number element
-					number = inkex.etree.Element(inkex.addNS('text', 'svg'))
-					number.text = str(idx+1)
-					number.set('x', str(nx))
-					number.set('y', str(ny))
-					number.set('font-size', self.options.fontsize)
-					number.set('text-anchor', textAnchor)
-					numberLayer.append(number)
+				# create the number element
+				text = inkex.TextElement()
+				text.text = str(idx+1)
+				text.set('x', str(nx))
+				text.set('y', str(ny))
+				text.set('font-size', str(self.options.fontsize))
+				text.set('text-anchor', textAnchor)
+				numberLayer.add(text)
 
-				# hide the original path if specified in options
-				if self.options.hidepath == 'true':
-					path.set('display', 'none')
+			# hide the original path if specified in options
+			if self.options.hidepath:
+				path.style['display'] = 'none'
 
 	
 	def getXY(self, vertex):
 		# split vertex info into command and list of parameters
 		cmd, params = vertex
+		
+		# Handle path commands with coordinate parameters
 		if cmd == 'M' or cmd == 'L':
-			'''Not necessary to check for 'm' and/or 'l',
-			because pathparser returns only absolute coordinates'''
-			x,y = params[0], params[1]
+			# Move to or Line to
+			if len(params) >= 2:
+				x, y = params[0], params[1]
+			else:
+				# Handle case with insufficient parameters
+				return (0, 0)
+		elif cmd == 'H':
+			# Horizontal line - only has x coordinate
+			if len(params) >= 1:
+				x = params[0]
+				# Use previous y (not implemented, return 0)
+				y = 0
+			else:
+				return (0, 0)
+		elif cmd == 'V':
+			# Vertical line - only has y coordinate
+			if len(params) >= 1:
+				# Use previous x (not implemented, return 0)
+				x = 0
+				y = params[0]
+			else:
+				return (0, 0)
+		elif cmd == 'Z' or cmd == 'z':
+			# Close path - no coordinates
+			return (0, 0)
 		else:
-			'''We are looking at a curved path, or cubic Bezier curve. The last
-			coordinate pair in the parameters list determine the point's position,
-			see: http://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands'''
-			x,y = params[-2:]
-		return (x,y)
+			# Curve commands (C, S, Q, T, A)
+			if len(params) >= 2:
+				x, y = params[-2:]
+			else:
+				# Handle case with insufficient parameters
+				return (0, 0)
+				
+		return (x, y)
 
 
 	def findFreeQuadrants(self, current, previous, next = None):
@@ -175,15 +196,11 @@ class ConnectTheDots(inkex.Effect):
 			else:
 				return self.BOTTOM_RIGHT
 		else:
-			# reference is on the right side
+			# reference is on the left side
 			if y2 > y1:
 				return self.TOP_LEFT
 			else:
 				return self.BOTTOM_LEFT
 
-
-
-# create effect instance and apply it.
 if __name__ == '__main__':
-    e = ConnectTheDots()
-    e.affect()
+    ConnectTheDots().run()
