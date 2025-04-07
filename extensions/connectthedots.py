debd0ea4e20513260bb0bb6e1904b23dc3f50a6b
jedena <blackmlm@gmail.com>
2025-04-07 11:14:56 -0500

    Modified to work on 1.4

--- extensions/connectthedots.py
+++ extensions/connectthedots.py
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Fixed to work on Inkscape 1.4 and later by Jose Garza
 '''
 
 from random import randint, choice
